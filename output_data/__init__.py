af6abe980864e1f51465d499f69e9f1ff13083db
ALMA ZANNUBA ARIFAH <alma.zannuba@trilogi.ac.id>
2026-02-03 23:35:30 +0700

    Add project description and author details
    Added project metadata and documentation in __init__.py

---
+++ output_data/__init__.py
@@ -0,0 +1,29 @@
+"""
+
+PPDL - Prediksi Pemakaian Daya Listrik
+PPDL - Electrical Power Consumption Prediction
+
+Academic Project Title:
+Prediksi Pemakaian Daya Listrik Pada Rumah Tangga di Jakarta Berbasis IoT
+dengan Menggunakan Metode Fuzzy Time Series
+
+Prediction of Household Electrical Power Consumption in Jakarta
+Using the Fuzzy Time Series Method
+
+Property of:
+Universitas Trilogi | Teknik Informatika | Program Sarjana (Strata 1)
+Trilogi University | Informatics Engineering | Bachelor Degree
+
+Author:
+Alma Zannuba Arifah | 19107011
+
+Supervisors:
+Ir. Yaddarabulah, M.Kom., Ph.D.
+Opitasari, S.Si., M.Kom.
+
+Module:
+Configuration Package
+
+Version: 1.0.1
+Created: December 2025
+"""
